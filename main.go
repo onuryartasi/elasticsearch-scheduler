6adab131f4b43a9aec01c75c8a9ffdb39a64e8e1
Onur YARTAŞI <onuryartasi@live.com>
2019-09-26 10:14:55 +0300

    added dummy data generator and local elasticsearch

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"log"
+	"strings"
 
 	"github.com/elastic/go-elasticsearch/v8"
 )
@@ -10,17 +12,34 @@ import (
 
 
 func main() {
-	request esapi.DeleteByQueryRequest{Index:"openshift*",Query:}
-	esapi.DeleteByQuery
-	cfg := elasticsearch.Config{
-		Addresses: []string{
-			"http://elasticsearch.bilyoner.com:9200",
-		},
-	}
-	es, _ := elasticsearch.NewClient(cfg)
-	log.Println(elasticsearch.Version)
-	log.Println(es.Info())
-	es.DeleteByQuery.WithQuery()
 
+	//cfg := elasticsearch.Config{
+	//	Addresses: []string{
+	//		"http://elasticsearch.bilyoner.com:9200",
+	//	},
+	//}
+	//es, _ := elasticsearch.NewClient(cfg)
+	es, _ := elasticsearch.NewDefaultClient()
+
+
+	//todo: index create with spesific shard number
+
+	req := esapi.IndexRequest{Index:"test",Body:strings.NewReader("{\"title\":\"test\"}"),WaitForActiveShards:"1"}
+	res, err := req.Do(context.Background(),es)
+	if err != nil {
+		log.Fatalf("%s",err)
+	}
+	defer res.Body.Close()
+	log.Printf("%s",res)
+
+	////todo: delete index
+	//deleteReq := esapi.DeleteRequest{Index:"test"}
+	//res, err = deleteReq.Do(context.Background(),es)
+	//
+	//if err != nil {
+	//	log.Fatalf("%s",err)
+	//}
+	//defer res.Body.Close()
+	//fmt.Printf("%s",res)
 
 }
